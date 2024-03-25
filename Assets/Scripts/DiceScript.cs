97c35ec58dd3377b096cf0b52146dad2ffa55c65
Chefaee <muffinxmuffins@gmail.com>
2024-03-25 23:33:46 +0100

    Added Functionality to UI-Scripts
    Add:
       - Theoretical functionality to GeneralUIScript.cs, DiceScript.cs
    
    ToDo:
       - Functionality and communication between UI Menu and GeneralUIScript.cs
       - Keep-Toggle Functionality for DiceScript.cs

--- Assets/Scripts/DiceScript.cs
+++ Assets/Scripts/DiceScript.cs
@@ -1,30 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
+using System;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager.Requests;
 
 public class DiceScript : MonoBehaviour
 {
     [SerializeField] GameObject wholeDiceGameObject;
-    [SerializeField] GameObject diceForm;
-    [SerializeField] GameObject diceEyes;
-    [SerializeField] TMPro.TextMeshPro eyesTMP;
-    [SerializeField] TMPro.TextMeshPro numberPOfEyesTMP;
+    [SerializeField] Image diceForm;
+    [SerializeField] Image diceEyes;
+    [SerializeField] TextMeshProUGUI eyesStaticTMP;
+    [SerializeField] TextMeshProUGUI numberOfEyesTMP;
     [SerializeField] Toggle keepDice;
+
+    // public string diceType; // get dicetype from generalUIScript
+    public string spriteName;
+    // public int possibleEyes; // get possible eyes from generalUIScript
+    public string spriteEyesName;
+    private int randomEye;
+
+    [SerializeField] GameObject parentUI;
+    private GeneralUIScript generalUIScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Initialize color
-        // https://stackoverflow.com/questions/39486497/change-the-ui-image-using-script-in-unity-c-sharp
-
-
-        // Disable Button
-        // Let the dice rotate 360° and maybe 3d rota
-        // Randomize eyes
-        // Load the eye image...
-        // Add +1 to m_round
-        // Enable the button
+        generalUIScript = parentUI.GetComponent<GeneralUIScript>();
+        throwDice();
     }
 
+    public void throwDice()
+    {
+        // Testing purposes ~
+        spriteName = "Green"; // remove this later, gets overwritten by generaluiscript
+
+        string diceType = generalUIScript.diceType;
+        int possibleEyes = generalUIScript.numberOfEyes;
+
+        // Initialize color
+        Sprite spriteForm = Resources.Load<Sprite>("Images/" + diceType + "/" + spriteName);
+        diceForm.sprite = spriteForm;
+
+
+        // Let the dice rotate 360° and maybe 3d rota
+        // https://stackoverflow.com/questions/28648071/rotate-object-in-unity-3d
+        // https://stackoverflow.com/questions/63194973/how-to-rotate-a-game-object-in-runtime-in-unity
+        // diceForm.transform.Rotate(Vector3.right * 20f * Time.deltaTime); // x, y, z
+
+        // Randomize eyes
+        // https://stackoverflow.com/questions/2706500/how-do-i-generate-a-random-integer-in-c
+        System.Random rnd = new System.Random();
+        randomEye = rnd.Next(1, possibleEyes + 1);
+        spriteName = randomEye.ToString();
+
+        // Load the eye image...
+        Sprite spriteEyes = Resources.Load<Sprite>("Images/" + diceType + "/Eyes/" + spriteName);
+        diceEyes.sprite = spriteEyes;
+    }
 }
--- Assets/Scripts/GeneralUIScript.cs
+++ Assets/Scripts/GeneralUIScript.cs
@@ -1,27 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GeneralUIScript : MonoBehaviour
 {
-    [SerializeField] TMPro.TextMeshPro roundStaticTMP;
-    [SerializeField] TMPro.TextMeshPro roundNumberTMP;
-    [SerializeField] Button newThrow;
-    [SerializeField] TMPro.TextMeshPro eyesSumStaticTMP;
-    [SerializeField] TMPro.TextMeshPro eyesSumNumberTMP;
+    public int numberOfDice;
+    public int numberOfEyes;
+    public string diceType;
 
-    [SerializeField] GameObject dice1;
-    [SerializeField] GameObject dice2;
-    [SerializeField] GameObject dice3;
-    [SerializeField] GameObject dice4;
-    [SerializeField] GameObject dice5;
-    [SerializeField] GameObject dice6;
+    [SerializeField] List<string> colourList;
+
+    private List<DiceScript> diceScritpList;
+
+    [SerializeField] TextMeshProUGUI roundStaticTMP;
+    [SerializeField] TextMeshProUGUI roundNumberTMP;
+    [SerializeField] Button newThrow;
+    [SerializeField] TextMeshProUGUI eyesSumStaticTMP;
+    [SerializeField] TextMeshProUGUI eyesSumNumberTMP;
+
+    [SerializeField] List<GameObject> diceList;
+
+    private DiceScript dice1Script;
+    private DiceScript dice2Script;
+    private DiceScript dice3Script;
+    private DiceScript dice4Script;
+    private DiceScript dice5Script;
+    private DiceScript dice6Script;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Init. Cube Colours
+        SetUpLists();
+        SetUpDice();
     }
 
+    private void SetUpLists()
+    {
+        diceScritpList = new List<DiceScript>();
+        diceScritpList.Add(dice1Script);
+        diceScritpList.Add(dice2Script);
+        diceScritpList.Add(dice3Script);
+        diceScritpList.Add(dice4Script);
+        diceScritpList.Add(dice5Script);
+        diceScritpList.Add(dice6Script);
+    }
+
+    private void SetUpDice()
+    {
+        // Get to know the number of dice first to enable/disable...
+        for (int i = 0; i < numberOfDice; i++)
+        {
+            // Make the dice visible
+            diceList[i].SetActive(true);
+
+            // Access dice scripts
+            // https://www.youtube.com/watch?v=Y7pp2gzCzUI
+            diceScritpList[i] = diceList[i].GetComponent<DiceScript>();
+
+            // Init. dice Colours
+            diceScritpList[i].spriteName = colourList[i];
+        }
+    }
 }
