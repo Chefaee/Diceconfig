d6d6140db263567a57d02ac1d5bad7bc46dd219f
Chefaee <muffinxmuffins@gmail.com>
2024-03-25 17:43:55 +0100

    Added Basic UI-Layout
    Add:
       - Basic UI-Layout (Menu, "Game" scene)
       - Basic Scripts for each Dice and Layout
    
    ToDo:
       - Script for Menu

---
+++ Assets/Scripts/DiceScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceScript : MonoBehaviour
+{
+    [SerializeField] GameObject wholeDiceGameObject;
+    [SerializeField] GameObject diceForm;
+    [SerializeField] GameObject diceEyes;
+    [SerializeField] TMPro.TextMeshPro eyesTMP;
+    [SerializeField] TMPro.TextMeshPro numberPOfEyesTMP;
+    [SerializeField] Toggle keepDice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize color
+        // https://stackoverflow.com/questions/39486497/change-the-ui-image-using-script-in-unity-c-sharp
+
+
+        // Disable Button
+        // Let the dice rotate 360° and maybe 3d rota
+        // Randomize eyes
+        // Load the eye image...
+        // Add +1 to m_round
+        // Enable the button
+    }
+
+}
---
+++ Assets/Scripts/GeneralUIScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralUIScript : MonoBehaviour
+{
+    [SerializeField] TMPro.TextMeshPro roundStaticTMP;
+    [SerializeField] TMPro.TextMeshPro roundNumberTMP;
+    [SerializeField] Button newThrow;
+    [SerializeField] TMPro.TextMeshPro eyesSumStaticTMP;
+    [SerializeField] TMPro.TextMeshPro eyesSumNumberTMP;
+
+    [SerializeField] GameObject dice1;
+    [SerializeField] GameObject dice2;
+    [SerializeField] GameObject dice3;
+    [SerializeField] GameObject dice4;
+    [SerializeField] GameObject dice5;
+    [SerializeField] GameObject dice6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Init. Cube Colours
+    }
+
+}
